709e232a22b7a72c0f4a85d4d0ab773ac4fb2a15
jljones78 <jljones78@gmail.com>
2020-03-03 15:45:42 -0600

    fixed scrape_mars script

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,14 +1,16 @@
-def scrape():
+# import dependencies
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+import requests
+import pymongo
+import pprint
+from pymongo import MongoClient
+from selenium import webdriver
+from splinter import Browser
 
-    # import dependencies
-    import pandas as pd
-    from bs4 import BeautifulSoup as bs
-    import requests
-    import pymongo
-    import pprint
-    from pymongo import MongoClient
-    from selenium import webdriver
-    from splinter import Browser
+
+def scrape():
+    scrape_results_dict = {}
 
     # scrape news site for title and paragraph text, save off to variables
     news_url = "https://mars.nasa.gov/news/"
@@ -17,12 +19,10 @@ def scrape():
     driver.implicitly_wait(10)
     html = driver.page_source
     news_soup = bs(html, "html.parser")
-
     news_title = (news_soup.find("div", class_="list_text")).find("a").text
-    print(news_title)
-
     news_paragraph = news_soup.find("div", class_="article_teaser_body").text
-    print(news_paragraph)
+    scrape_results_dict["news_title"] = news_title
+    scrape_results_dict["news_paragraph"] = news_paragraph
 
     # find featured Mars image from JPL, save full-size image url
     # note: this url doesn't seem to pull up a Mars image any longer
@@ -33,14 +33,14 @@ def scrape():
     html = driver.page_source
     img_soup = bs(html, "html.parser")
 
-    # build url from sytle tag
+    # build url from style tag
     image_url = (
         img_soup.find("article")["style"]
         .replace("background-image: url(", "")
         .replace(");", "")[1:-1]
     )
     featured_image_url = base_jpl_url + image_url
-    print(featured_image_url)
+    scrape_results_dict["featured_image_url"] = featured_image_url
 
     # scrape latest mars weather from twitter
     mars_wx_url = "https://twitter.com/MarsWxReport"
@@ -49,17 +49,15 @@ def scrape():
     wx_soup = bs(html, "html.parser")
 
     mars_weather = wx_soup.find_all("div", class_="js-tweet-text-container")[0].text
+    scrape_results_dict["mars_weather"] = mars_weather
 
     # scrape Mars facts website, display as html table str using pandas
     mars_facts_url = "https://space-facts.com/mars/"
     mars_df = pd.read_html(mars_facts_url)[0]
 
-    ##  print html string
+    # print html string
     mars_facts_html_str = mars_df.to_html()
-    pprint.pprint(mars_facts_html_str)
-
-    ## render html
-    mars_facts_html = mars_df.to_html("mars_facts.html")
+    scrape_results_dict["mars_facts"] = mars_facts_html_str
 
     # scrape USGS Astrogeology site for high res photos of hemispheres
     usgs_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
@@ -78,6 +76,6 @@ def scrape():
         browser.visit(hemisphere_url)
         temp_dict["img_url"] = browser.find_by_css('img[class="wide-image"]')["src"]
         hemisphere_dict.append(temp_dict)
-    pprint.pprint(hemisphere_dict)
-    driver.close()
-    browser.quit()
+    scrape_results_dict["hemisphere_dict"] = hemisphere_dict
+
+    print(scrape_results_dict)
